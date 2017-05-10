37219bebda1a73c30428463c580d54a89b85b093
Yongping.Li <lypkiller@gmail.com>
2017-05-10 17:12:55 +0800

    download the mp3 files to a folder

--- python/wordbook_youdao.py
+++ python/wordbook_youdao.py
@@ -15,21 +15,26 @@ def setup_proxy_for_urllib2(proxies=None):
         opener = urllib2.build_opener(proxy)
         urllib2.install_opener(opener)
 
-def download_word(word_name):
+def download_word(word_name, folder=None):
     # type 1: English
     # type 2: American
     base_url = 'http://dict.youdao.com/dictvoice?audio='+word_name+'&type=2'
-    download_mp3(base_url, word_name+".mp3")
+    download_mp3(base_url, word_name+".mp3", folder)
 
-def download_words(words=[]):
+def download_words(words=[], folder=None):
     for word in words:
-        download_word(word)
+        download_word(word, folder)
 
-def download_mp3(url, saved_name, proxy=None):
+def download_mp3(url, saved_name, folder=None):
 
+    file_patch_name = saved_name
+    if folder is not None:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        file_patch_name = os.path.join(folder, saved_name)
     #urllib.urlretrieve('http://dict.youdao.com/dictvoice?audio=safari&type=1', "safari.mp3")
     mp3_file = urllib2.urlopen(url)
-    with open(saved_name, 'wb') as output:
+    with open(file_patch_name, 'wb') as output:
         output.write(mp3_file.read())
 
 
@@ -41,14 +46,17 @@ def play_mp3(filename, max_seconds=5):
     time.sleep(min(max_seconds, clip.seconds()))
     clip.stop()
 
-def play_words(words=[], dwell_time=1):
+def play_words(words=[], folder=None, dwell_time=1):
     for word in words:
-        play_mp3(word+".mp3")
-        time.sleep(1)
+        word_file_name = word+".mp3"
+        if folder is not None:
+            word_file_name = os.path.join(folder, word_file_name)
+        play_mp3(word_file_name)
+        time.sleep(dwell_time)
 
 #setup_proxy_for_urllib2({'http': 'www.example.com:80'})
-words = ['your','me','safari','test']
+words = ['your','I','i']
 
-download_words(words)
+download_words(words, "wordbook")
 
-play_words(words)
+play_words(words, "wordbook")
